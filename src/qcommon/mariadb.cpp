77a6eaf9dd4c52a6c90002a7c306f6508050c5c2
Tom <tomarrowtomarrow@hotmail.com>
2024-08-22 02:44:59 +0200

    Mariadb start thread

--- src/qcommon/mariadb.cpp
+++ src/qcommon/mariadb.cpp
@@ -1,17 +1,18 @@
 
+
 #include "qcommon.h"
+#include <thread>
 
 cvar_t* db_enabled;
-cvar_t* db_url;
-cvar_t* db_username;
-cvar_t* db_password;
+static cvar_t* db_url;
+static cvar_t* db_username;
+static cvar_t* db_password;
 
-void DB_Init() {
+static std::thread* dbThread;
 
-	db_enabled = Cvar_Get("db_enabled", "0", CVAR_ARCHIVE);
-	db_url = Cvar_Get("db_url", "", CVAR_ARCHIVE); // "jdbc:mariadb://localhost:3306/todo"
-	db_username = Cvar_Get("db_username", "", CVAR_ARCHIVE);
-	db_password = Cvar_Get("db_password", "", CVAR_ARCHIVE);
+
+static void DB_BackgroundThread() {
+
 
 	sql::Driver* driver = sql::mariadb::get_driver_instance();
 
@@ -21,5 +22,15 @@ void DB_Init() {
 
 	// Establish Connection
 	std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
-
 }
+
+void DB_Init() {
+
+	db_enabled = Cvar_Get("db_enabled", "0", CVAR_ARCHIVE);
+	db_url = Cvar_Get("db_url", "", CVAR_ARCHIVE); // "jdbc:mariadb://localhost:3306/todo"
+	db_username = Cvar_Get("db_username", "", CVAR_ARCHIVE);
+	db_password = Cvar_Get("db_password", "", CVAR_ARCHIVE);
+
+	//dbThread = new std::thread(DB_BackgroundThread);
+}
+
