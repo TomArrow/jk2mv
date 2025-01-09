aeda4d2498d3e491e2b6bbf09fbb48e7ec365eb8
Tom <tomarrowtomarrow@hotmail.com>
2025-01-09 20:04:49 +0100

    mvsdk and remove optimizedpredict

--- src/client/cl_main.cpp
+++ src/client/cl_main.cpp
@@ -3467,7 +3467,7 @@ void CL_Init( void ) {
 
 	Cvar_Get ("password", "", CVAR_USERINFO);
 	Cvar_Get ("cg_predictItems", "1", CVAR_USERINFO | CVAR_ARCHIVE );
-	Cvar_Get ("cg_optimizedPredict", "0", CVAR_ARCHIVE );
+	//Cvar_Get ("cg_optimizedPredict", "0", CVAR_ARCHIVE );
 
 	// cgame might not be initialized before menu is used
 	Cvar_Get ("cg_viewsize", "100", CVAR_ARCHIVE );
