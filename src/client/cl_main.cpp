TomArrow/jk2mv

commit 7956225b42ec6d5b8a391fe2b3c489be73c16a69

- Let it properly load dl_ pk3s even if demo doesnt have correct checksumfeed (like in my reframes) - cl_fpsguess 3 works in float now

src/qcommon/files.cpp
@@ -273,6 +273,7 @@ static	int			fs_packFiles;			// total number of files in packs
 
 static int fs_fakeChkSum;
 static int fs_checksumFeed;
+static qboolean fs_checksumFeedSet;
 
 typedef union qfile_gus {
 	FILE*		o;
@@ -345,6 +346,7 @@ static const char	*fs_serverPakNames[MAX_SEARCH_PATHS];			// pk3 names
 static int			fs_numServerReferencedPaks;
 static int			fs_serverReferencedPaks[MAX_SEARCH_PATHS];			// checksums
 static const char	*fs_serverReferencedPakNames[MAX_SEARCH_PATHS];		// pk3 names
+static qboolean		fs_referencedPaksChanged;
 
 // last valid game folder used
 char lastValidBase[MAX_OSPATH];
@@ -4599,6 +4601,7 @@ checksums to see if any pk3 files need to be auto-downloaded.
 */
 void FS_PureServerSetReferencedPaks( const char *pakSums, const char *pakNames ) {
 	int		i, c, d = 0;
+	int		tmp;
 
 	Cmd_TokenizeString( pakSums );
 
@@ -4608,7 +4611,11 @@ void FS_PureServerSetReferencedPaks( const char *pakSums, const char *pakNames )
 	}
 
 	for ( i = 0 ; i < c ; i++ ) {
-		fs_serverReferencedPaks[i] = atoi( Cmd_Argv( i ) );
+		tmp = atoi(Cmd_Argv(i));
+		if (tmp != fs_serverReferencedPaks[i]) {
+			fs_referencedPaksChanged = qtrue;
+		}
+		fs_serverReferencedPaks[i] = tmp;
 	}
 
 	for ( i = 0 ; i < (int)ARRAY_LEN(fs_serverReferencedPakNames) ; i++ ) {
@@ -4633,7 +4640,11 @@ void FS_PureServerSetReferencedPaks( const char *pakSums, const char *pakNames )
 		Com_Printf(S_COLOR_YELLOW "WARNING: Corrupted server pak references\n");
 	}
 
-	fs_numServerReferencedPaks = MIN(c, d);
+	tmp = MIN(c, d);
+	if (tmp != fs_numServerReferencedPaks) {
+		fs_referencedPaksChanged = qtrue;
+	}
+	fs_numServerReferencedPaks = tmp;
 }
 
 /*
@@ -4691,6 +4702,8 @@ void FS_Restart2( int checksumFeed, qboolean inPlace ) {
 
 	// set the checksum feed
 	fs_checksumFeed = checksumFeed;
+	fs_checksumFeedSet = qtrue;
+	fs_referencedPaksChanged = qfalse;
 
 	// clear pak references
 	FS_ClearPakReferences(0);
@@ -4745,7 +4758,7 @@ restart if necessary
 =================
 */
 qboolean FS_ConditionalRestart( int checksumFeed ) {
-	if( fs_gamedirvar->modified || checksumFeed != fs_checksumFeed ) {
+	if( fs_gamedirvar->modified || checksumFeed != fs_checksumFeed || !fs_checksumFeedSet || fs_referencedPaksChanged) {
 		FS_Restart( checksumFeed );
 		return qtrue;
 	}

src/client/cl_parse.cpp
@@ -785,7 +785,8 @@ void CL_ParseSnapshot( msg_t *msg ) {
 			
 
 			static int oldCommandTime = 0;
-			static int downFallVelocityDeltasAndMsecDeltas[FPS_GUESS_METHOD3_MAX_FRAMEAVG_COUNT][2]{};
+			static float downFallVelocityDeltas[FPS_GUESS_METHOD3_MAX_FRAMEAVG_COUNT]{};
+			static float downFallMsecDeltas[FPS_GUESS_METHOD3_MAX_FRAMEAVG_COUNT]{};
 			static int downFallVelocityDeltasAndMsecDeltasIndex = 0;
 			static int lastGravity = -999;
 			//static float effectiveGravities[FPS_GUESS_METHOD3_MSEC_LIMIT]{}; // The velocity deltas resulting from various fps settings based on curent gravity. Lucky for us, this is the same regardless of current downspeed as gravity is constant and we are working in 1 dimension (Z axis)
@@ -819,10 +820,10 @@ void CL_ParseSnapshot( msg_t *msg ) {
 
 				if ((/*(!frameHasLevitation && !lastFrameHadLevitation) || */ (isMovementDown && cls.fpsGuess.lastMovementDown)) && cl.snap.ps.groundEntityNum == ENTITYNUM_NONE && cl.snap.ps.velocity[2] < cls.fpsGuess.lastVelocity[2] && commandTimeDelta <= 999) {
 
-					int downFallDelta = fabsf(cl.snap.ps.velocity[2] - cls.fpsGuess.lastVelocity[2]) + 0.5f;
+					float downFallDelta = fabsf(cl.snap.ps.velocity[2] - cls.fpsGuess.lastVelocity[2]);// +0.5f;
 					if (downFallDelta <= 999) {
-						downFallVelocityDeltasAndMsecDeltas[downFallVelocityDeltasAndMsecDeltasIndex % actualFrameCount][0] = downFallDelta;
-						downFallVelocityDeltasAndMsecDeltas[downFallVelocityDeltasAndMsecDeltasIndex++ % actualFrameCount][1] = commandTimeDelta;
+						downFallVelocityDeltas[downFallVelocityDeltasAndMsecDeltasIndex % actualFrameCount] = downFallDelta;
+						downFallMsecDeltas[downFallVelocityDeltasAndMsecDeltasIndex++ % actualFrameCount] = commandTimeDelta;
 						cls.fpsGuess.lastFrameWasMeasured = qtrue;
 
 						// Check if we are possibly sliding along some surface
@@ -845,10 +846,10 @@ void CL_ParseSnapshot( msg_t *msg ) {
 			// Actual analysis.
 			if (downFallVelocityDeltasAndMsecDeltasIndex >= actualFrameCount) {
 				int totalTimeDelta = 0;
-				int totalVelocityDelta = 0;
+				float totalVelocityDelta = 0;
 				for (int frame = 0; frame < actualFrameCount; frame++) {
-					totalVelocityDelta += downFallVelocityDeltasAndMsecDeltas[frame][0];
-					totalTimeDelta += downFallVelocityDeltasAndMsecDeltas[frame][1];
+					totalVelocityDelta += downFallVelocityDeltas[frame];
+					totalTimeDelta += downFallMsecDeltas[frame];
 				}
 				float effectiveMeasuredGravity = 1000.0f*(float)totalVelocityDelta/(float)totalTimeDelta;
 

commit 89f596deb8a0ddb1a06806095ffae942ad86d1d9

"Fix" dumbass bug I introduced with 7956225b42ec6d5b8a391fe2b3c489be73c16a69 File system would restart for the client even tho the server was running in the same .exe, so the game would lose all file handles. Yuck.

src/client/cl_main.cpp
@@ -1446,7 +1446,7 @@ void CL_FlushMemory( qboolean disconnecting ) {
 		// change checksum feed so that next FS_ConditionalRestart()
 		// works when connecting back to the same server
 		clc.checksumFeed = 0;
-		FS_Restart(clc.checksumFeed);
+		FS_Restart(clc.checksumFeed, qtrue);
 	}
 
 	CL_StartHunkUsers();
@@ -2060,7 +2060,7 @@ Restart the filesystem
 */
 
 void CL_FS_Restart_f( void ) {
-	FS_Restart( clc.checksumFeed ); //xD
+	FS_Restart( clc.checksumFeed, qfalse ); //xD
 }
 
 /*
@@ -2273,7 +2273,7 @@ void CL_DownloadsComplete( void ) {
 	if (clc.downloadRestart) {
 		clc.downloadRestart = qfalse;
 
-		FS_Restart(clc.checksumFeed); // We possibly downloaded a pak, restart the file system to load it
+		FS_Restart(clc.checksumFeed, qfalse); // We possibly downloaded a pak, restart the file system to load it
 
 		// inform the server so we get new gamestate info
 		CL_AddReliableCommand( "donedl" );

src/qcommon/files.cpp
@@ -3798,7 +3798,7 @@ static void FS_Restart_f( void ) {
 		Com_Printf( "^3WARNING: Cannot restart file system due to active file handles for pk3 files inside of modules.\n" );
 		return;
 	}
-	FS_Restart2( fs_checksumFeed, qtrue );
+	FS_Restart2( fs_checksumFeed, qtrue, qfalse);
 }
 
 //===========================================================================
@@ -4797,6 +4797,14 @@ void FS_PureServerSetReferencedPaks( const char *pakSums, const char *pakNames )
 		fs_referencedPaksChanged = qtrue;
 	}
 	fs_numServerReferencedPaks = tmp;
+
+	if (com_sv_running->integer) {
+		// fs_referencedPaksChanged is a dumb hack so we can play demos that reference dl_ pk3s but have a corrupt/non-existent checksumfeed (0)
+		// unfortunately a running server in a client will already have loaded the filesystem right but not set the referenced paks serverside,
+		// therefore we would force a file system restart and kill all open game files....
+		// TODO and TLDR: This is a shitty workaround for shittily reframed demos. Fix it all at some point.
+		fs_referencedPaksChanged = qfalse; 
+	}
 }
 
 /*
@@ -4847,14 +4855,14 @@ void FS_InitFilesystem( void ) {
 FS_Restart
 ================
 */
-void FS_Restart2( int checksumFeed, qboolean inPlace ) {
+void FS_Restart2( int checksumFeed, qboolean inPlace, qboolean checksumFeedIsBeingUnset) {
 
 	// free anything we currently have loaded
 	FS_Shutdown(qfalse, inPlace);
 
 	// set the checksum feed
 	fs_checksumFeed = checksumFeed;
-	fs_checksumFeedSet = qtrue;
+	fs_checksumFeedSet = (qboolean)(!checksumFeedIsBeingUnset);
 	fs_referencedPaksChanged = qfalse;
 
 	// clear pak references
@@ -4875,7 +4883,7 @@ void FS_Restart2( int checksumFeed, qboolean inPlace ) {
 			Cvar_Set("fs_gamedirvar", lastValidGame);
 			lastValidBase[0] = '\0';
 			lastValidGame[0] = '\0';
-			FS_Restart(checksumFeed);
+			FS_Restart(checksumFeed, qfalse);
 			Com_Error( ERR_DROP, "Invalid game folder" );
 			return;
 		}
@@ -4899,8 +4907,8 @@ void FS_Restart2( int checksumFeed, qboolean inPlace ) {
 
 }
 
-void FS_Restart( int checksumFeed ) {
-	FS_Restart2( checksumFeed, qfalse );
+void FS_Restart( int checksumFeed, qboolean checksumFeedIsBeingUnset) {
+	FS_Restart2( checksumFeed, qfalse, checksumFeedIsBeingUnset);
 }
 
 /*
@@ -4911,7 +4919,7 @@ restart if necessary
 */
 qboolean FS_ConditionalRestart( int checksumFeed ) {
 	if( fs_gamedirvar->modified || checksumFeed != fs_checksumFeed || !fs_checksumFeedSet || fs_referencedPaksChanged) {
-		FS_Restart( checksumFeed );
+		FS_Restart( checksumFeed, qfalse);
 		return qtrue;
 	}
 	return qfalse;

src/qcommon/qcommon.h
@@ -800,8 +800,8 @@ void	FS_InitFilesystem (void);
 void	FS_Shutdown( qboolean closemfp, qboolean keepModuleFiles );
 
 qboolean	FS_ConditionalRestart( int checksumFeed );
-void	FS_Restart2( int checksumFeed, qboolean inPlace );
-void	FS_Restart( int checksumFeed );
+void	FS_Restart2( int checksumFeed, qboolean inPlace, qboolean checksumFeedIsBeingUnset);
+void	FS_Restart( int checksumFeed, qboolean checksumFeedIsBeingUnset);
 // shutdown and restart the filesystem so changes to fs_gamedir can take effect
 
 const char	**FS_ListFiles( const char *directory, const char *extension, int *numfiles );

src/server/sv_init.cpp
@@ -672,7 +672,7 @@ Ghoul2 Insert End
 	sv.checksumFeed = ( ((int) rand() << 16) ^ rand() ) ^ Com_Milliseconds();
 
 	FS_PureServerSetReferencedPaks("", "");
-	FS_Restart( sv.checksumFeed );
+	FS_Restart( sv.checksumFeed, qfalse);
 
 	CM_LoadMap( va("maps/%s.bsp", server), qfalse, &checksum );
 
@@ -983,7 +983,8 @@ void SV_Init (void) {
 	Cvar_Set("sv_master2", "master.jk2mv.org");
 	sv_master[2] = Cvar_Get("sv_master3", "master.jkhub.org", CVAR_ROM);
 	Cvar_Set("sv_master3", "master.jkhub.org");
-	sv_master[3] = Cvar_Get("sv_master4", "", CVAR_ARCHIVE | CVAR_GLOBAL);
+	sv_master[3] = Cvar_Get("sv_master4", "master.ouned.de", CVAR_ROM);
+	Cvar_Set("sv_master4", "master.ouned.de");
 	sv_master[4] = Cvar_Get("sv_master5", "", CVAR_ARCHIVE | CVAR_GLOBAL);
 	sv_master[5] = Cvar_Get("sv_master6", "", CVAR_ARCHIVE | CVAR_GLOBAL);
 	sv_master[6] = Cvar_Get("sv_master7", "", CVAR_ARCHIVE | CVAR_GLOBAL);